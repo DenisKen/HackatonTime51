90f48084e4bb457e1a236d98b52e0bf4381fbb41
Denis Ken <denis.kenno@gmail.com>
2021-01-16 23:54:55 -0300

    Created class_detail wireframe

--- lib/screens/class_detail.dart
+++ lib/screens/class_detail.dart
@@ -14,17 +14,33 @@ class ClassDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarBack(),
-      body: Container(child: Column(children: [
-        panelCover(),
-        CircleAvatar(
-          radius: 40,
-        )
-      ])),
+      body: Stack(children: [
+        SingleChildScrollView(
+          child: Container(
+              child: Column(children: [
+            _panelCover(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 22, vertical: 21),
+              child: Column(
+                children: [
+                  _rowMentor(),
+                  Padding(padding: EdgeInsets.only(bottom: 20)),
+                  _details(),
+                  Padding(padding: EdgeInsets.only(bottom: 20)),
+                  _subscribes()
+                ],
+              ),
+            )
+          ])),
+        ),
+        _button()
+      ]),
     );
   }
 
-  Widget panelCover() {
+  Widget _panelCover() {
     return Container(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
         color: Colors.grey,
         height: 200,
         child: Column(
@@ -39,5 +55,73 @@ class ClassDetail extends StatelessWidget {
             )
           ],
         ));
+  }
+
+  Widget _rowMentor() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 40,
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+            ),
+            Text('Professor'),
+          ],
+        ),
+        Container(
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: Center(child: Text('20')),
+        )
+      ],
+    );
+  }
+
+  Widget _details() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Detalhes'),
+        Padding(padding: EdgeInsets.only(bottom: 10)),
+        Text('Lorem asidja sdjioa jiojdoi ajdoiasj oiajdojao jaodj aodjaid')
+      ],
+    );
+  }
+
+  Widget _subscribes() {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 20,
+        ),
+        Container(
+          height: 20,
+          width: 30,
+          decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(5))),
+          child: Center(child: Text('11')),
+        )
+      ],
+    );
+  }
+
+  Widget _button() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 50,
+        width: SizeConfig.safeBlockHorizontal * 100,
+        color: Colors.grey,
+        child: Center(child: Text('Register')),
+      ),
+    );
   }
 }
